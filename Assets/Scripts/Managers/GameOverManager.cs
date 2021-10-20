fd31bce9b189a86e56d099518bea207036065a7f
Atthiyyath <56459274+Atthiyya22194@users.noreply.github.com>
2021-10-21 00:24:29 +0700

    Add files via upload

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;       
+    public float restartDelay = 5f;
+    private bool isDead = false;
+
+    Animator anim;                          
+    float restartTimer;                    
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            if (!isDead)
+            {
+                anim.SetTrigger("GameOver");
+                isDead = true;
+            }
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
